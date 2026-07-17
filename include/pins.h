452d8c5f69425539d1d71e326768fc264f05b237
Ayush Pandey <aryupandey789@gmail.com>
2026-07-17 17:19:46 +0530

    Add pins.h for motor driver, encoders, OLED, and battery

---
+++ include/pins.h
@@ -0,0 +1,27 @@
+#ifndef PINS_H
+#define PINS_H
+
+// Motor Driver
+#define MOTOR_LEFT_IN1 26
+#define MOTOR_LEFT_IN2 27
+#define MOTOR_RIGHT_IN1 14
+#define MOTOR_RIGHT_IN2 12
+
+#define MOTOR_LEFT_PWM 13
+#define MOTOR_RIGHT_PWM 15
+
+// Encoders
+#define ENCODER_LEFT_A 34
+#define ENCODER_LEFT_B 35
+
+#define ENCODER_RIGHT_A 32
+#define ENCODER_RIGHT_B 33
+
+// OLED
+#define OLED_SDA 21
+#define OLED_SCL 22
+
+// Battery
+#define BATTERY_PIN 39
+
+#endif
