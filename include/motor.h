98b3cc2b4933830377bcd57e74edbf3583bf5e2d
Ayush Pandey <aryupandey789@gmail.com>
2026-07-17 17:20:09 +0530

    Add motor header file with function declarations

---
+++ include/motor.h
@@ -0,0 +1,16 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+void motorInit();
+
+void moveForward(int speed);
+
+void moveBackward(int speed);
+
+void turnLeft();
+
+void turnRight();
+
+void stopMotor();
+
+#endif
