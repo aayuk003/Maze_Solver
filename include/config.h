e465a014feba8c525160802ebacd7ed665020535
Ayush Pandey <aryupandey789@gmail.com>
2026-07-17 17:19:14 +0530

    Update config.h

---
+++ include/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MAZE_SIZE 16
+
+#define MAX_SPEED 255
+#define MIN_SPEED 80
+
+#define KP 1.2
+#define KI 0.0
+#define KD 0.15
+
+#define CELL_SIZE_MM 180
+
+#endif
