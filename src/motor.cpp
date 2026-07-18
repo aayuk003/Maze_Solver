409cdcc4275fadbf9fe60c274c51193e1a666d1f
Ayush Pandey <aryupandey789@gmail.com>
2026-07-18 22:10:53 +0530

    Update motor.cpp

---
+++ src/motor.cpp
@@ -0,0 +1,26 @@
+#include "motor.h"
+#include "pins.h"
+
+void motorInit()
+{
+}
+
+void moveForward(int speed)
+{
+}
+
+void moveBackward(int speed)
+{
+}
+
+void turnLeft()
+{
+}
+
+void turnRight()
+{
+}
+
+void stopMotor()
+{
+}
