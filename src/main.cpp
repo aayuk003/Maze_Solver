93d9472baeca57f2e9222951e979c44a8e8b2011
Ayush Pandey <aryupandey789@gmail.com>
2026-07-18 22:15:16 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,45 @@
 #include <Arduino.h>
 
 #include "motor.h"
-#include "sensor.h"
 #include "encoder.h"
+#include "sensor.h"
+#include "display.h"
 #include "maze.h"
 #include "floodfill.h"
-#include "display.h"
+#include "battery.h"
+#include "imu.h"
+#include "wifi.h"
 
 void setup()
 {
+    Serial.begin(115200);
+
     motorInit();
-    sensorInit();
+
     encoderInit();
+
+    sensorInit();
+
+    imuInit();
+
+    batteryInit();
+
     displayInit();
+
     initMaze();
+
+    wifiInit();
+
+    Serial.println("Micromouse Robot Started");
 }
 
 void loop()
 {
     solveMaze();
+
+    showStatus();
+
+    sendTelemetry();
+
+    delay(10);
 }
